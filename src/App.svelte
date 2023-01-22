c4e22848267548fac951ab588a3adf3a5a4b6ed1
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-22 09:54:56 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+import CryptoJS from 'crypto-js';
+
+let name;
+let password;
+// $: log = `${name}${password}`;
+
+var encrypted = CryptoJS.AES.encrypt({password}, "Secret Passphrase");
+
+
+</script>
+
+<fieldset class="fieldset">
+	
+	<label>
+		<input bind:value={name} placeholder="name" />
+		Name
+	</label>
+	<label>
+		<input bind:value={password} placeholder="password" />
+		Password
+	</label>
+  <br>
+{#if encrypted}
+{encrypted}
+{/if}
+
+</fieldset>
+
+
+
+<style>
+.fieldset{position: absolute; top: 1em; left:1em; background-color: white;}
+</style>
