b35cb17610b6ae63103d3d6d8d5a56d3abfb5a80
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-26 15:17:23 +0100

    svelte conform $

--- src/App.svelte
+++ src/App.svelte
@@ -12,18 +12,18 @@
 	let salt3 = "=\/88v->#Jtu[mv->#Mg3@mXjR$\}#Mg3@mXjR$\}<W"
 
 	// combine name + salt1, combine pass + salt2
-	let saltedName = `${name}${salt1}`;
-	let saltedPass = `${pass}${salt2}`;
+	$: saltedName = `${name}${salt1}`;
+	$: saltedPass = `${pass}${salt2}`;
 
 	// hash salted name and pass
-	let hashName = keccak512(saltedName);
-	let hashPass = keccak512(saltedPass)
+	$: hashName = keccak512(saltedName);
+	$: hashPass = keccak512(saltedPass)
 	
 	// combine hashName, hashpass and salt3
-	let combination = `${hashPass}${salt3}${hashName}`;
+	$: combination = `${hashPass}${salt3}${hashName}`;
 	
 	// hash from combination
-	let hash = keccak512(combination);
+	$: hash = keccak512(combination);
 
 	// later there is a QR Code generated from this hash. The QR Code can be reversed into the hash without encryption.
 	// Only the owner of the right credentials (name+pass)can authenticate himself to be the owner of the QR-Code.
