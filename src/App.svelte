9a511ba2e0dc60cc142b8c17c0c74027510bfd72
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-29 23:00:04 +0100

    Made with ChatGPT

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,94 @@
 <script>
-	import QR from 'svelte-qr';
-	import { keccak512 } from 'js-sha3';
-	
-	// name and pass
-	let name = '';
-  	let pass = '';
+  
+    // Use a hash function such as SHA256 to create a hash from the input string
+    const hash = (str) => {
+  return window.crypto.subtle.digest("SHA-256", new TextEncoder().encode(str)).then(hash => {
+    const dataView = new DataView(hash);
+    let result = "";
+    for (let i = 0; i < dataView.byteLength; i += 4) {
+      result += dataView.getUint32(i).toString(16).padStart(8, "0");
+    }
+    return result;
+  });
+};
 
-	// salt name, salt pass
-	let salt1 = "v->#Mg3@mXjR$\}<WJtu[m~m<=\/88";
-	let salt2 = "Jtu[m~m<Mg3@mXjR$\}<W";
-	let salt3 = "=\/88v->#Jtu[mv->#Mg3@mXjR$\}#Mg3@mXjR$\}<W"
+  
+  // Use an encryption algorithm such as RSA to sign the data using the private key
+  const sign = async (privateKeyJwk, data) => {
+    const privateKey = await window.crypto.subtle.importKey(
+  "jwk",
+  privateKeyJwk,
+  { name: "RSA-OAEP", hash: { name: "SHA-256" } },
+  false,
+  ["decrypt", "sign"]
+);
 
-	// combine name + salt1, combine pass + salt2
-	$: saltedName = `${name}${salt1}`;
-	$: saltedPass = `${pass}${salt2}`;
+  return window.crypto.subtle.sign({ name: "RSASSA-PKCS1-v1_5", hash: { name: "SHA-256" } }, privateKey, new TextEncoder().encode(data));
+};
 
-	// hash salted name and pass
-	$: hashName = keccak512(saltedName);
-	$: hashPass = keccak512(saltedPass)
-	
-	// combine hashName, hashpass and salt3
-	$: combination = `${hashPass}${salt3}${hashName}`;
-	
-	// hash from combination
-	$: hash = keccak512(combination);
 
-	// later there is a QR Code generated from this hash. The QR Code can be reversed into the hash without encryption.
-	// Only the owner of the right credentials (name+pass)can authenticate himself to be the owner of the QR-Code.
-	// (Zero-Knowledge-Proof)
+  const generateKeyPair = () => {
+    // Use a key generation algorithm such as RSA to generate a public/private key pair
+    return window.crypto.subtle.generateKey(
+
+    
+      {
+        name: "RSA-OAEP",
+        modulusLength: 2048,
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+        hash: { name: "SHA-256" }
+      },
+      true,
+      ["encrypt", "decrypt"]
+    ).then(keyPair => {
+      return window.crypto.subtle.exportKey("jwk", keyPair.privateKey)
+        .then(privateKeyJwk => {
+          return window.crypto.subtle.exportKey("jwk", keyPair.publicKey)
+            .then(publicKeyJwk => {
+              return {
+                privateKey: privateKeyJwk,
+                publicKey: publicKeyJwk
+              };
+            });
+        });
+    });
+  };
+  
+  let name = "";
+  let password = "";
+
+  // Login form submission handler
+  const handleLogin = async () => {
+  // Salt and hash the credentials
+  const salt = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const hashedCredentials = await hash(`${name}${password}${salt}`);
+
+  // Generate a key pair
+  const { publicKey, privateKey } = await generateKeyPair();
+
+  // Sign the public key with the hashed credentials
+  const signature = await sign(privateKey, hashedCredentials);
+
+  // Store the public key and signature in the browser's local storage
+  localStorage.setItem("publicKey", JSON.stringify(publicKey));
+  localStorage.setItem("signature", btoa(String.fromCharCode.apply(null, new Uint8Array(signature))));
+
+  // Redirect the user to the main app
+  window.location.href = "/app";
+};
+  
+  const loggedIn = localStorage.getItem("publicKey") && localStorage.getItem("signature");
 </script>
 
 
 
-<!-- Markup-->
-<fieldset class="fieldset" align="center">
-	
-	<label>
-	<input id="name" bind:value={name} placeholder="name" />
-	</label>
+{#if !loggedIn} <!-- If the user is not logged in, display the login form -->
 
-	<label>
-	<input bind:value={pass} placeholder="passphrase" />
-	</label>
-
-  {#if combination}
-  <div>
-  Generate QR from:<br>
-  		<div style:color="red">
-
-		"{combination}" <!--shows the content of let combination-->
-
-  		</div>
-  </div>
-  {/if}
-
-	{#key `${hash}`}
-	<div class="qr">
-		<QR text={hash} version={-1} /> <!--Generates a QR Code from let hash-->
-  	</div>
-	{/key}
-
-</fieldset>
-
-
-
-<style>
-.fieldset{position: absolute; top: 1em; left:1em; background-color: white;}
-.qr{margin: 5%;}
-</style>
+  <form on:submit|preventDefault={handleLogin}>
+    <input type="text" bind:value={name} placeholder="Name" />
+    <input type="password" bind:value={password} placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+{:else} <!--If the user is logged in, display the main app-->
+  <p>Welcome to the main app!</p>
+{/if}
