92448b0ccb0e0863c95c800678bb0ec0eb93497f
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-30 16:46:28 +0100

    ts to js

--- src/App.svelte
+++ src/App.svelte
@@ -1,94 +1,80 @@
 <script>
-  
-    // Use a hash function such as SHA256 to create a hash from the input string
-    const hash = (str) => {
-  return window.crypto.subtle.digest("SHA-256", new TextEncoder().encode(str)).then(hash => {
-    const dataView = new DataView(hash);
-    let result = "";
-    for (let i = 0; i < dataView.byteLength; i += 4) {
-      result += dataView.getUint32(i).toString(16).padStart(8, "0");
-    }
-    return result;
-  });
-};
-
-  
-  // Use an encryption algorithm such as RSA to sign the data using the private key
-  const sign = async (privateKeyJwk, data) => {
-    const privateKey = await window.crypto.subtle.importKey(
-  "jwk",
-  privateKeyJwk,
-  { name: "RSA-OAEP", hash: { name: "SHA-256" } },
-  false,
-  ["decrypt", "sign"]
-);
-
-  return window.crypto.subtle.sign({ name: "RSASSA-PKCS1-v1_5", hash: { name: "SHA-256" } }, privateKey, new TextEncoder().encode(data));
-};
-
-
-  const generateKeyPair = () => {
-    // Use a key generation algorithm such as RSA to generate a public/private key pair
-    return window.crypto.subtle.generateKey(
-
-    
-      {
-        name: "RSA-OAEP",
-        modulusLength: 2048,
-        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-        hash: { name: "SHA-256" }
-      },
-      true,
-      ["encrypt", "decrypt"]
-    ).then(keyPair => {
-      return window.crypto.subtle.exportKey("jwk", keyPair.privateKey)
-        .then(privateKeyJwk => {
-          return window.crypto.subtle.exportKey("jwk", keyPair.publicKey)
-            .then(publicKeyJwk => {
-              return {
-                privateKey: privateKeyJwk,
-                publicKey: publicKeyJwk
-              };
-            });
-        });
-    });
+  let user = {};
+  let storageLimit = 1048576; // 1 MB in bytes
+  const hashAlgorithm = 'SHA-256';
+  const curve = 'P-256';
+  const signingAlgorithm = {
+    name: 'ECDSA',
+    hash: { name: hashAlgorithm },
   };
-  
-  let name = "";
-  let password = "";
 
-  // Login form submission handler
-  const handleLogin = async () => {
-  // Salt and hash the credentials
-  const salt = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  const hashedCredentials = await hash(`${name}${password}${salt}`);
+  async function hashPassword(password, salt) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password + salt);
+    const hash = await window.crypto.subtle.digest(hashAlgorithm, data);
+    return Array.from(new Uint8Array(hash))
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('');
+  }
 
-  // Generate a key pair
-  const { publicKey, privateKey } = await generateKeyPair();
+  async function createKeyPair() {
+    const keyPair = await window.crypto.subtle.generateKey(
+      { name: 'ECDSA', namedCurve: curve },
+      true,
+      ['sign', 'verify']
+    );
+    const publicKey = await window.crypto.subtle.exportKey('raw', keyPair.publicKey);
+    const privateKey = await window.crypto.subtle.exportKey('jwk', keyPair.privateKey);
+    return { publicKey, privateKey };
+  }
 
-  // Sign the public key with the hashed credentials
-  const signature = await sign(privateKey, hashedCredentials);
+  async function signPublicKey(privateKey, publicKey) {
+    const signature = await window.crypto.subtle.sign(
+      signingAlgorithm,
+      privateKey,
+      publicKey
+    );
+    return Array.from(new Uint8Array(signature))
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('');
+  }
 
-  // Store the public key and signature in the browser's local storage
-  localStorage.setItem("publicKey", JSON.stringify(publicKey));
-  localStorage.setItem("signature", btoa(String.fromCharCode.apply(null, new Uint8Array(signature))));
+  async function storeKeys(user, publicKey, signature) {
+    let storageSize = JSON.stringify(localStorage).length;
+    if (storageSize + JSON.stringify({ user, publicKey, signature }).length > storageLimit) {
+      console.log('Local storage size limit reached');
+      return;
+    }
+    localStorage.setItem(user, JSON.stringify({ publicKey, signature }));
+    console.log('Stored keys:', localStorage);
+  }
 
-  // Redirect the user to the main app
-  window.location.href = "/app";
-};
-  
-  const loggedIn = localStorage.getItem("publicKey") && localStorage.getItem("signature");
+  function handleSubmit(e) {
+    user = { ...e.target.elements };
+    let salt = 'random_salt';
+    hashPassword(user.password, salt).then(hashedPassword => {
+      user.password = hashedPassword;
+      createKeyPair().then(({ publicKey, privateKey }) => {
+        signPublicKey(privateKey, publicKey).then(signature => {
+          storeKeys(user.username, publicKey, signature);
+        });
+      });
+    });
+  }
+
+  function emptyTemplate() {
+    localStorage.clear();
+  }
 </script>
 
-
-
-{#if !loggedIn} <!-- If the user is not logged in, display the login form -->
-
-  <form on:submit|preventDefault={handleLogin}>
-    <input type="text" bind:value={name} placeholder="Name" />
-    <input type="password" bind:value={password} placeholder="Password" />
+{#if Object.keys(user).length}
+  <p>Welcome to the main app!</p>
+{:else}
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
     <button type="submit">Login</button>
   </form>
-{:else} <!--If the user is logged in, display the main app-->
-  <p>Welcome to the main app!</p>
 {/if}
+
+<style></style>
